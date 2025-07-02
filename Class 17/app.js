0e25e2e9553e66f366a6be100b089f84649d05f4
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2025-07-02 20:20:28 +0500

    class 17 upload

---
+++ Class 17/app.js
@@ -0,0 +1,46 @@
+// if condition
+// let myName = "Khan";
+// if(myName == "Khan"){
+//     console.log("Door is opened for you");    
+// }
+// IF Else Condition
+// let personName = "Moiz";
+// if (personName == "Sabih"){
+//     console.log("Hi, door is opened for you!")
+// }else{
+//     console.log("Sorry, door is locked for you!")
+// }
+// Else If Conditon
+var housePerson = "Uzair";
+if (housePerson == "Owais") {
+    console.log("Gate is opened!");
+}
+else if (housePerson == "Faisal") {
+    console.log("Gate is opened!");
+}
+else if (housePerson == "Tariq") {
+    console.log("Gate is opened!");
+}
+else {
+    console.log("Gate is locked!");
+}
+var gharDinner = "Biryani";
+if (gharDinner == "Biryani") {
+    console.log("Main khana ghar pa khao ga..");
+}
+else {
+    console.log("Main khana bahir ja kar khao gi..");
+}
+// Ternary Operator
+var age = 17;
+age >= 18 ? console.log("Cnic ban jaega") : console.log("Cnic nai bana ga");
+// Loops in JS
+// for (let i = 1; i <= 20; i++){
+//     console.log("My name is Khan")
+// }
+// While loop
+var i = 1;
+while (i <= 10) {
+    console.log("I is smaller than 10");
+    i++;
+}
---
+++ Class 17/app.ts
@@ -0,0 +1,57 @@
+// if condition
+
+// let myName = "Khan";
+
+// if(myName == "Khan"){
+//     console.log("Door is opened for you");    
+// }
+
+// IF Else Condition
+// let personName = "Moiz";
+// if (personName == "Sabih"){
+//     console.log("Hi, door is opened for you!")
+// }else{
+//     console.log("Sorry, door is locked for you!")
+// }
+
+// Else If Conditon
+
+let housePerson = "Uzair";
+
+if(housePerson == "Owais"){
+    console.log("Gate is opened!")
+}else if(housePerson == "Faisal"){
+    console.log("Gate is opened!")
+}else if(housePerson == "Tariq"){
+    console.log("Gate is opened!")
+}else{
+    console.log("Gate is locked!")
+}
+
+let gharDinner = "Biryani";
+
+if (gharDinner == "Biryani"){
+    console.log("Main khana ghar pa khao ga..")
+}else{
+    console.log("Main khana bahir ja kar khao gi..")
+}
+
+
+// Ternary Operator
+let age = 17;
+
+age >= 18 ? console.log("Cnic ban jaega") : console.log("Cnic nai bana ga");
+
+
+// Loops in JS
+
+// for (let i = 1; i <= 20; i++){
+//     console.log("My name is Khan")
+// }
+
+// While loop
+let i = 1;
+while(i <= 100){
+    console.log("I is smaller than 10");
+    i++;
+}
