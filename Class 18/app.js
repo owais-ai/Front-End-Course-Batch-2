7811d141505a12a98a80069b9ab2f42c5c732836
Owais Khan <160103547+owais-ai@users.noreply.github.com>
2025-07-02 21:10:15 +0500

    class 19 upload

---
+++ Class 18/app.js
@@ -0,0 +1,9 @@
+var classNames = ["Mudabbir", "Shoaib", "Muneer", "Hasan", "Anas"];
+classNames.push("Ismael");
+console.log(classNames);
+classNames.pop();
+console.log(classNames);
+classNames.shift();
+console.log(classNames);
+classNames.unshift("Moiz");
+console.log(classNames);
---
+++ Class 18/app.ts
@@ -0,0 +1,15 @@
+let classNames = ["Mudabbir", "Shoaib", "Muneer", "Hasan", "Anas"]
+
+classNames.push("Ismael")
+
+console.log(classNames);
+
+classNames.pop()
+
+console.log(classNames)
+
+classNames.shift();
+console.log(classNames)
+
+classNames.unshift("Moiz");
+console.log(classNames)
